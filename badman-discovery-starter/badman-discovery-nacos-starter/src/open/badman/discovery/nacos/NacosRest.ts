8030ec260e3ce438bc38c74846a1fb631460688e
hetq <hetq@anmed.com.cn>
2023-11-28 13:21:04 +0800

    optimize some bugs

--- badman-discovery-starter/badman-discovery-nacos-starter/src/open/badman/discovery/nacos/NacosRest.ts
+++ badman-discovery-starter/badman-discovery-nacos-starter/src/open/badman/discovery/nacos/NacosRest.ts
@@ -1,8 +1,8 @@
 
 
-import {Initializing, Logging, SingletonObjectFactory2} from "badman-core";
+import {Beans, Initializing, Logging, SingletonObjectFactory2} from "badman-core";
 import {HttpClientResponse, RestTemplate} from "badman-rest-starter";
-import {Logger} from "log4js";
+import log4js,{Logger} from "log4js";
 import NacosDiscovery from "./NacosDiscovery";
 import NacosInstanceProperties from "./NacosInstanceProperties";
 import NoSuchServer from "./NoSuchServer";
@@ -10,23 +10,71 @@ import RestRequestError from "./RestRequestError";
 import RestRequestParam from "./RestRequestParam";
 
 
+
+
+
 export default class NacosRest implements Initializing{
 
 	private logger:Logger;
-	private readonly restTemplate:RestTemplate;
-	private readonly nacosDiscovery:NacosDiscovery;
+	private restTemplate:RestTemplate;
+	private nacosDiscovery:NacosDiscovery;
 
-	constructor () {
-		this.logger = SingletonObjectFactory2.Instance<Logging>(Logging.name).logger(NacosRest.name);
+	constructor (restTemplate?:RestTemplate,nacosDiscovery?:NacosDiscovery,logger?:Logger) {
 
-		this.restTemplate = SingletonObjectFactory2.Instance<RestTemplate>(RestTemplate.name);
+		if(logger){
+			this.logger = logger;
+		}else{
+			this.logger = this.deduceLogger(NacosRest.name);
+		}
+
+		if(restTemplate){
+			this.restTemplate = restTemplate;
+		}else{
+			this.deduceRestTemplate();
+		}
+
 		if(!this.restTemplate){
 			throw new Error('RestTemplate can not be null');
 		}
-		this.nacosDiscovery = SingletonObjectFactory2.Instance<NacosDiscovery>(NacosDiscovery.name);
+
+		if(nacosDiscovery){
+			this.nacosDiscovery = nacosDiscovery;
+		}else{
+			this.deduceNacosDiscovery();
+		}
+
 		if(!this.nacosDiscovery){
 			throw new Error('NacosDiscovery can not be null');
 		}
+	}
+
+	private deduceLogger(category?:string){
+		let logging:Logging = SingletonObjectFactory2.Instance<Logging>(Logging.name) || SingletonObjectFactory2.Instance<Logging>('logging');
+		if(!logging){
+			logging = Beans.Instance<Logging>(Logging.name) || Beans.Instance<Logging>('logging');
+		}
+		if(logging){
+			return logging.logger(category || 'default');
+		}else{
+			return log4js.getLogger(category || 'default');
+		}
+	}
+
+	private deduceRestTemplate(){
+
+		let restTemplate:RestTemplate = SingletonObjectFactory2.Instance<RestTemplate>(RestTemplate.name) || SingletonObjectFactory2.Instance<RestTemplate>('restTemplate');
+		if(!restTemplate){
+			restTemplate = Beans.Instance<RestTemplate>(RestTemplate.name) || Beans.Instance<RestTemplate>('restTemplate');
+		}
+		this.restTemplate = restTemplate;
+	}
+
+	private deduceNacosDiscovery(){
+		let nacosDiscovery:NacosDiscovery = SingletonObjectFactory2.Instance<NacosDiscovery>(NacosDiscovery.name) || SingletonObjectFactory2.Instance<NacosDiscovery>('nacosDiscovery');
+		if(!nacosDiscovery){
+			nacosDiscovery = Beans.Instance<NacosDiscovery>(NacosDiscovery.name) || Beans.Instance<NacosDiscovery>('nacosDiscovery');
+		}
+		this.nacosDiscovery = nacosDiscovery;
 	}
 
 	async afterInitialized () {
@@ -43,7 +91,7 @@ export default class NacosRest implements Initializing{
 		let address:string = await this.nacosDiscovery.obtainInstance(instanceProperties);
 		if(address){
 			let uri:string = '/';
-			if(request.serviceNameForPath && !address.includes(request.serviceName)){
+			if(request.serviceNameForPath){
 				uri += request.serviceName;
 			}
 			if(request.namespace){
